6fd27b6f2417dad49003bb5cb9f57c853693998c
Mikayel <abachyanm@gmail.com>
2022-11-15 02:14:45 +0400

    refactoring routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import Avia from "./pages/Avia";
 import Info from "./pages/Info";
 
@@ -8,8 +8,9 @@ function App() {
     <div className="App">
         <div className={"container"}>
             <Routes>
+                <Route index element={<Navigate to={`/avia`} replace/>}/>
                 <Route path='avia/'>
-                    <Route index  element={<Avia/>}/>
+                    <Route path={''} element={<Avia/>}/>
                     <Route path='info' element={<Info/>}/>
                 </Route>
             </Routes>
